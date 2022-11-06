761cc0f72e47f01dbdcd44a3eb4f2abb387a63b1
Chris Ritter <chriswritter@gmail.com>
2022-11-06 18:06:09 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace CodeKY_Knowledge_Check_2
+    {
+
+    internal class Program
+        {
+        static void Main(string[] args)
+            {
+            Console.WriteLine(" ██████╗  █████╗ ███╗   ███╗███████╗    ██╗     ██╗███████╗████████╗\r\n██╔════╝ ██╔══██╗████╗ ████║██╔════╝    ██║     ██║██╔════╝╚══██╔══╝\r\n██║  ███╗███████║██╔████╔██║█████╗      ██║     ██║███████╗   ██║   \r\n██║   ██║██╔══██║██║╚██╔╝██║██╔══╝      ██║     ██║╚════██║   ██║   \r\n╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗    ███████╗██║███████║   ██║   \r\n ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝    ╚══════╝╚═╝╚══════╝   ╚═╝   \r\n                                                                    ");
+
+            Console.WriteLine("How many records do you want to add? ");
+            var numberOfRecords = int.Parse(Console.ReadLine());
+
+
+
+            var gameList = new List<Game>();
+            for (int i = 0; i < numberOfRecords; i++)
+                {
+                // In this loop, populate the object's properties using Console.ReadLine()
+                var entry = new Game();
+
+                Console.WriteLine("Name of Game: ");
+                entry.gameName = Console.ReadLine();
+
+                Console.WriteLine("Enter the Genre: ");
+                entry.genre = Console.ReadLine();
+
+                gameList.Add(new Game() { gameName = entry.gameName, genre = entry.genre });
+                }
+
+            // Print out the list of records using Console.WriteLine()
+            foreach (var entry in gameList)
+                Console.WriteLine("\n\rGame: " + entry.gameName.ToUpper() + "\nGenre: " + entry.genre.ToUpper() + "\n\r");
+            }
+        }
+    }
