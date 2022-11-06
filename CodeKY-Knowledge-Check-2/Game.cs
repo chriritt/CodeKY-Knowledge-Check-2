3c2f99dceb5f7975dc94e1154efea8289d32dc46
Chris Ritter <112297081+chriritt@users.noreply.github.com>
2022-11-06 18:11:10 -0500

    Add files via upload

---
+++ CodeKY-Knowledge-Check-2/Game.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeKY_Knowledge_Check_2
+    {
+    public class Game
+        {
+        public string gameName { get; set; }
+        public string genre { get; set; }
+        public int trophies { get; set; }
+        }
+    }
---
+++ CodeKY-Knowledge-Check-2/GameConsole.cs
@@ -0,0 +1,7 @@
+﻿namespace CodeKY_Knowledge_Check_2
+    {
+    public abstract class GameConsole
+        {
+        public string WhichConsole { get; set; }
+        }
+    }
